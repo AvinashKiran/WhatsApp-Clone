0154ab4f5931aabb97fa2e69ad3f51894cb15ada
Frezy Stone Mboumba <FrezyStone@macbook-pro-de-frezy.local>
2016-07-13 16:57:20 -0500

    Added Tab Bar Item Icons

---
+++ WhatsAppClone/ConversationsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ConversationsTableViewCell.swift
+//  WhatsAppClone
+//
+//  Created by Frezy Stone Mboumba on 7/13/16.
+//  Copyright © 2016 Frezy Stone Mboumba. All rights reserved.
+//
+
+import UIKit
+
+class ConversationsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WhatsAppClone/SettingsTableViewController.swift
@@ -0,0 +1,42 @@
+//
+//  SettingsTableViewController.swift
+//  WhatsAppClone
+//
+//  Created by Frezy Stone Mboumba on 7/13/16.
+//  Copyright © 2016 Frezy Stone Mboumba. All rights reserved.
+//
+
+import UIKit
+
+class SettingsTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+   
+
+}
